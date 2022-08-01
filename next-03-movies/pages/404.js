88b71663946b39ab76f163f7d09ab39b73b40ad9
Alex Charco <alex.charco.25@gmail.com>
2022-07-31 23:43:41 -0500

    Page 404

---
+++ next-03-movies/pages/404.js
@@ -0,0 +1,17 @@
+import Layout from "../components/Layout";
+import styles from "../styles/404.module.css";
+import Link from "next/link";
+
+const page404 = () => {
+  return (
+    <Layout title="404 Amnxiery ahead!!">
+      <div className={styles.error}>
+        <h1 className={styles.title}>Amnxiety ahead</h1>
+        <p className={styles.par} >Go back cowboy</p>
+        <Link href="/">To main</Link>
+      </div>
+    </Layout>
+  );
+};
+
+export default page404;
